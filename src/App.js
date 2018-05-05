7a79bc956f05e2d87775840e727ef32074b8cb12
ali1129js <ali1129js@gmail.com>
2018-05-05 17:06:00 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * @Author: Ali
+ * @Date:   2018-05-05T17:04:42+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-05-05T17:05:15+02:00
+ */
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    <h1> init </h1>
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/**
+ * @Author: Ali
+ * @Date:   2018-05-05T17:04:27+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-05-05T17:04:32+02:00
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
