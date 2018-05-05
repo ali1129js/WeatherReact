44000a54903051528010bb164eac4e658417ace8
ali1129js <ali1129js@gmail.com>
2018-05-05 19:19:27 +0200

    input values added to the api endpoint

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: Ali
  * @Date:   2018-05-05T17:04:42+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-05T19:10:03+02:00
+ * @Last modified time: 2018-05-05T19:14:27+02:00
  */
 import React, { Component, Fragment } from 'react'
 import Form from './Components/Form'
@@ -10,7 +10,9 @@ const API_KEY = '32941f4c12936fc51133cb69ca8f8b8a'
 class App extends Component {
   getWeather = async (e) => {
     e.preventDefault()
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`)
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
     const data = await api_call.json()
     console.log(data)
   }
