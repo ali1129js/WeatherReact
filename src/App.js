9080b2c4adbbea1e74d6f37d84fd2ee59401ee02
ali1129js <ali1129js@gmail.com>
2018-05-05 19:46:26 +0200

    passed data to new component weather

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@
  * @Author: Ali
  * @Date:   2018-05-05T17:04:42+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-05T19:14:27+02:00
+ * @Last modified time: 2018-05-05T19:45:33+02:00
  */
 import React, { Component, Fragment } from 'react'
 import Form from './Components/Form'
+import Weather from './Components/Weather'
 const API_KEY = '32941f4c12936fc51133cb69ca8f8b8a'
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: undefined
+    }
+  }
   getWeather = async (e) => {
     e.preventDefault()
     const city = e.target.elements.city.value
@@ -15,11 +27,27 @@ class App extends Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
     const data = await api_call.json()
     console.log(data)
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    })
   }
   render() {
     return (
     <Fragment>
       <Form getWeather={this.getWeather}/>
+      <Weather
+        temperature={this.state.temperature}
+        humidity={this.state.humidity}
+        city={this.state.city}
+        country={this.state.country}
+        description={this.state.description}
+        error={this.state.error}
+      />
     </Fragment>
     )
   }
---
+++ src/Components/Weather.js
@@ -0,0 +1,15 @@
+/**
+ * @Author: Ali
+ * @Date:   2018-05-05T19:41:09+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-05-05T19:45:55+02:00
+ */
+import React, { Component} from 'react'
+class Weather extends Component {
+  render(){
+    return (
+      <div className="w">Weather Component</div>
+    )
+  }
+}
+export default Weather
