7ec864fa24ec5bf00adae8c0795998ed1d6da8d8
ali1129js <ali1129js@gmail.com>
2018-05-09 13:22:02 +0200

    conditions everywhere

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -2,16 +2,20 @@
  * @Author: Ali
  * @Date:   2018-05-05T19:41:09+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-09T12:47:58+02:00
+ * @Last modified time: 2018-05-09T13:21:15+02:00
  */
 import React, { Component} from 'react'
 class Weather extends Component {
   render(){
+    const {city,country,temperature,humidity,description,error} = this.props
+    //we'll use the AND &&  operator
     return (
       <div className="w">
-        {this.props.temperature}
-        {this.props.humidity}
-        {this.props.description}
+        { city && country && <p> Location: {city},country </p> }
+        { temperature && <p> Temperature:  {temperature} </p>}
+        { humidity && <p> Humidity:{humidity} </p>}
+        { description && <p> Condition:{this.props.description} </p>}
+        { error && <p> {error} </p>}
       </div>
     )
   }
