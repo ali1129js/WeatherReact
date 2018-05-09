8c382a0f8d7860a8486fb6f4ae7d8d10e119f055
ali1129js <ali1129js@gmail.com>
2018-05-09 13:31:21 +0200

    refactor weather into functional component

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -2,22 +2,18 @@
  * @Author: Ali
  * @Date:   2018-05-05T19:41:09+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-09T13:21:15+02:00
+ * @Last modified time: 2018-05-09T13:30:07+02:00
  */
 import React, { Component} from 'react'
-class Weather extends Component {
-  render(){
-    const {city,country,temperature,humidity,description,error} = this.props
-    //we'll use the AND &&  operator
-    return (
-      <div className="w">
-        { city && country && <p> Location: {city},country </p> }
-        { temperature && <p> Temperature:  {temperature} </p>}
-        { humidity && <p> Humidity:{humidity} </p>}
-        { description && <p> Condition:{this.props.description} </p>}
-        { error && <p> {error} </p>}
-      </div>
-    )
-  }
-}
+
+const Weather = props => (
+  <div className="w">
+    { props.city && props.country && <p> Location: {props.city},country </p> }
+    { props.temperature && <p> Temperature:  {props.temperature} </p>}
+    { props.humidity && <p> Humidity:{props.humidity} </p>}
+    { props.description && <p> Condition:{props.description} </p>}
+    { props.error && <p> {props.error} </p>}
+  </div>
+)
+
 export default Weather
