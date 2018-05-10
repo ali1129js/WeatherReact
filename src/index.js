d762464b0f44f339e4e53e553b1820363cba24a8
ali1129js <ali1129js@gmail.com>
2018-05-10 16:53:53 +0200

    removed all css

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@
  * @Author: Ali
  * @Date:   2018-05-05T17:04:27+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-09T15:39:09+02:00
+ * @Last modified time: 2018-05-10T16:53:35+02:00
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import registerServiceWorker from './registerServiceWorker'
 
