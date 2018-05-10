7d8f6742b25f99d49073b1f046a9ed7d1f97ab59
ali1129js <ali1129js@gmail.com>
2018-05-10 17:53:58 +0200

    styled-components test

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -2,19 +2,23 @@
  * @Author: Ali
  * @Date:   2018-05-05T19:41:09+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-10T16:51:34+02:00
+ * @Last modified time: 2018-05-10T16:59:52+02:00
  */
 import React from 'react'
-
+import styled from 'styled-components'
+const Content = styled.div`
+  background: papayawhip;
+  height: 30em;
+  width: 30em;`
 const Weather = props => (
-  <div>
+  <Content>
     { props.city && props.country && <p> Location:
       { props.city},{ props.country} </p> }
     { props.temperature && <p> Temperature:  {props.temperature} </p>}
     { props.humidity && <p> Humidity:{props.humidity} </p>}
     { props.description && <p> Condition:{props.description} </p>}
     { props.error && <p> {props.error} </p>}
-  </div>
+  </Content>
 )
 
 export default Weather
